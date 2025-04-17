46e5e70107591db26e100609946dc40176ebe66f
isom <smallsol.fa@gmail.com>
2025-04-17 23:10:29 +0900

    refactor: Lv.3 테스트코드 연습 2-3 NPE가 발생하던 부분을 찾아 예외처리 로직 개선

--- src/main/java/org/example/expert/domain/manager/service/ManagerService.java
+++ src/main/java/org/example/expert/domain/manager/service/ManagerService.java
@@ -35,8 +35,10 @@ public class ManagerService {
         Todo todo = todoRepository.findById(todoId)
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
 
-        if (!ObjectUtils.nullSafeEquals(user.getId(), todo.getUser().getId())) {
+        // getUser해서 null 인 경우를 확인해 InvalidRequestException을 던짐
+        if (todo.getUser()==null || !ObjectUtils.nullSafeEquals(user.getId(), todo.getUser().getId())) {
             throw new InvalidRequestException("담당자를 등록하려고 하는 유저가 일정을 만든 유저가 유효하지 않습니다.");
+            // 담당자를 등록하려고 하는 유저가 일정을 작성한 유저가 아닙니다.
         }
 
         User managerUser = userRepository.findById(managerSaveRequest.getManagerUserId())
