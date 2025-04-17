0d12bff3a2150bdd706b9a52583888317280267f
isom <smallsol.fa@gmail.com>
2025-04-17 13:45:50 +0900

    refactor : Lv.1 3.Validation 비밀번호 변경 부분 불필요한 코드 제거 및 UserChangePasswordRequest 부분 수정

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,12 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
+    // @Pattern 어노테이션으로 비밀번호 조건 검증 및 메세지 반환
     @NotBlank
+    @Pattern(regexp="^(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{8,}$",message = "새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.")
     private String newPassword;
+
+
+
 }
--- src/main/java/org/example/expert/domain/user/service/UserService.java
+++ src/main/java/org/example/expert/domain/user/service/UserService.java
@@ -25,12 +25,8 @@ public class UserService {
 
     @Transactional
     public void changePassword(long userId, UserChangePasswordRequest userChangePasswordRequest) {
-        if (userChangePasswordRequest.getNewPassword().length() < 8 ||
-                !userChangePasswordRequest.getNewPassword().matches(".*\\d.*") ||
-                !userChangePasswordRequest.getNewPassword().matches(".*[A-Z].*")) {
-            throw new InvalidRequestException("새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.");
-        }
 
+        // UserChangePasswordRequest부분에 @Pattern 어노테이션 사용으로 불필요한 코드 삭제
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new InvalidRequestException("User not found"));
 
